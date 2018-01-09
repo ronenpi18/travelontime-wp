a301090d1fd32cd9ed5d018bc1e3e94cec44ce5e
Ronen Pinhasov <ronen@Ronens-MacBook-Pro.local>
2018-01-10 01:26:17 +0200

    official web tripon

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 app.use(express.static(__dirname));
-
+var PORT = process.env.PORT || 3000
 app.get('/',function(req,res){
     res.sendfile(path.join(__dirname+'/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 
 
-app.listen(3000);
-
-console.log("Running at Port 3000");
+app.listen(PORT);
+console.log(PORT)
